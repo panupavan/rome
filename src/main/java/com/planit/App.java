e667d84247d51fd5de7af5b23294b71f751a3579
panupavan <116708870+panupavan@users.noreply.github.com>
2022-10-27 20:25:09 +0530

    a

---
+++ src/main/java/com/planit/App.java
@@ -0,0 +1,13 @@
+package com.planit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
